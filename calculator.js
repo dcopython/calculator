ab49e73f5fdb08fc21e594141837640dce46fc64
Daniel Co <dcopythontest@gmail.com>
2020-03-07 17:19:01 -0800

    second working iteration

--- calculator.js
+++ calculator.js
@@ -22,18 +22,27 @@ function operate(operator, numA, numB) {
     return window[operator](numA, numB);
 }
 
+function clearDisplay() {
+    currentDisplay.textContent = "";
+    document.body.append(currentDisplay);
+}
+
 //global vars
 const currentDisplay = document.querySelector("#currentdisplay");
-const totalDisplay = document.querySelector("#totaldisplay");
-let selectedOperator;
-let firstNumber;
+let currentArray = []; //track inputed numbers
+let numbersArray = []; //track joined numbers
+let operatorArray = []; //track operators
+let runningTotal;
 
 //display selected numbers when pressed
 const numButtons = document.querySelectorAll('.numbers');
 numButtons.forEach((button) => {
     button.addEventListener('click', function(e) {
+        currentArray.push(e.target.innerHTML);
+
         currentDisplay.textContent += e.target.innerHTML;
         document.body.append(currentDisplay);
+
     })
 })
 
@@ -41,41 +50,86 @@ numButtons.forEach((button) => {
 const operatorButtons = document.querySelectorAll('.operators');
 operatorButtons.forEach((button) => {
     button.addEventListener('click', function(e) {
-        selectedOperator = e.target.id; //update global var with selected operator
-    
-        firstNumber = currentDisplay.textContent; //save first number when operator selected
-    
-        totalDisplay.textContent = firstNumber; //update totalDisplay with currentDisplay content
-        totalDisplay.textContent += ` ${e.target.innerHTML} `; //add operator into totalDisplay
-        
-        currentDisplay.textContent = ""; //clear current display
-    
+        //join all selected numbers and push to numbersArray if there's numbers in currentArray
+        if (currentArray.length > 0) {
+            numbersArray.push(currentArray.join(''));
+        }
+
+        //clear the current array to prep for next number
+        currentArray = [];
+
+        //display operator selected and add to operatorArray
+        operatorArray.push(e.target.id);
+        currentDisplay.textContent += ` ${e.target.innerHTML} `;
         document.body.append(currentDisplay);
-        document.body.append(totalDisplay);
+
+        //if two numbers in numbersArray, calculate and show total, else wait for next number
+        if (numbersArray.length == 2) {
+            runningTotal = operate(operatorArray[0], numbersArray[0], numbersArray[1]);
+
+            numbersArray = [];
+            numbersArray.push(runningTotal);
+
+            currentDisplay.textContent = runningTotal;
+            document.body.append(currentDisplay);
+
+            //if operator selected again, calculate and show new operator
+            operatorArray = [];
+            operatorArray.push(e.target.id);
+            currentDisplay.textContent += ` ${e.target.innerHTML} `;
+            document.body.append(currentDisplay);
+        }
+
+        //if operator is selected first
+
+
     })
 })
 
 //when equals selected
 const equals = document.querySelector('#equals'); 
 equals.addEventListener('click', function(e) {
-    let secondNumber = currentDisplay.textContent; //save second number
-    //console.log(operate(selectedOperator, firstNumber, secondNumber))
+    //second number doesn't exist yet when equal button is clicked
+
+    //push second number if it's been inputted, then calculate
+    if (currentArray.length > 0) {
+        numbersArray.push(currentArray.join(''));
+    }
+    else { //if no second number inputted, return runningtotal in display
+        currentDisplay.textContent = runningTotal;
+        document.body.append(currentDisplay);
+    }
+
+    if (numbersArray.length == 2) {
+        runningTotal = operate(operatorArray[0], numbersArray[0], numbersArray[1]);
+
+        numbersArray = [];
+        numbersArray.push(runningTotal);
+
+        currentDisplay.textContent = runningTotal;
+        document.body.append(currentDisplay);
+    }
+    else {  //if equal is clicked with only first number
+        currentDisplay.textContent = numbersArray[0];
+        document.body.append(currentDisplay);
+    }
+
+    //if equal is clicked with no first or second number
+    if (numbersArray.length == 0) {
+        currentDisplay.textContent = "0";
+        document.body.append(currentDisplay);
+    }
     
-    //update totalDisplay after calculation
-    totalDisplay.textContent = operate(selectedOperator, firstNumber, secondNumber);
-    currentDisplay.textContent = "";
-    document.body.append(currentDisplay);
-    document.body.append(totalDisplay);
+    //leave runningtotal in currentArray in case more numbers are inputted onto total after pressing equal
+    currentArray = [];
+    currentArray.push(runningTotal);
 })
 
 //when clear selected
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', function(e) {
-    currentDisplay.textContent = "";
-    totalDisplay.textContent = "";
-    document.body.append(currentDisplay);
-    document.body.append(totalDisplay);
+    clearDisplay();
+    currentArray = [];
+    numbersArray = [];
+    operatorArray = [];
 })
-
-
-
